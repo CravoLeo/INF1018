ff8ca9ea3774092d2e3d569bd22f7a484f294018
Leonardo Cravo <lscravo5@gmail.com>
2025-09-19 00:58:16 +0000

    ex2 lista 7

--- INF1018/lab7/ex1.s
+++ INF1018/lab7/ex1.s
@@ -1,8 +1,5 @@
 /*
-#include <stdio.h>
-
 char S2[] = {65, 108, 111, 32, 123, 103, 97, 108, 101, 114, 97, 125, 33, 0};
-
 int main (void) {
   char *pc = S2;
   while (*pc)
@@ -10,24 +7,17 @@ int main (void) {
   printf("\n");
   return 0;
 }
-  */
-/*
-Dicionário
-Registrador variável
-rbx é pc
-eax é temp
 */
 
+.data
+S2:    .byte 65, 108, 111, 32, 123, 103, 97, 108, 101, 114, 97, 125, 33, 0
+Sf:  .string "%c"    /* primeira string de formato para printf */
+Sf2: .string "\n"    /* segunda string de formato para printf */
 
-.data #seg de dados
-char S2[] = {char S2[] = {65, 108, 111, 32, 123, 103, 97, 108, 101, 114, 97, 125, 33, 0};};
-S2: .byte 65, 108, 111, 32, 123, 103, 97, 108, 101, 114, 97, 125, 33, 0
-Sf: .string "%d\n"
-S_termina: .string "\n"
-.text #segmento de texto
-#int main(void){
-.global main #main é um símbolo global(toda função é global)
+.text
+.globl  main
 main:
+
 /********************************************************/
 /* mantenha este trecho aqui e nao mexa - prologo !!!   */
   pushq   %rbp
@@ -36,42 +26,40 @@ main:
   movq    %rbx, -8(%rbp)  /* guarda rbx */
   movq    %r12, -16(%rbp)  /* guarda r12 */
 /********************************************************/
-#char *pc = S2;
-movq $52,%rbx
 
-#while(*pc)
-loop_while:
-  cmpb $0, (%rbx)
-  je FORA_WHILE
+  movq  $S2, %r12  /* r12 = &S2 */
 
-#printf("%c",*pc++);
-#lembrar que *pc++ incrementa o conteúdo de pc
-#(*pc)++ incrementa o conteúdo de memória apontado pelo pc
-#    temp = *pc
-    movsbl (%rbx), %eax
-#printf
+L1:
+  cmpb  $0, (%r12)  /* if (*pc == 0) ? */
+  je  L2          /* goto L2 */
+
+
+  cmpb $123,(%r12)
+  je Lpula_print
+  cmpb $125,(%r12)
+  je Lpula_print
+
+  movsbl  (%r12), %eax    /* eax = *r12 (estendendo o byte para 32 bits */
+
 /*************************************************************/
-    /* este trecho imprime o valor de %eax (estraga %eax)  */
+/* este trecho imprime o valor de %eax (estraga %eax)  */
   movq    $Sf, %rdi    /* primeiro parametro (ponteiro)*/
   movl    %eax, %esi   /* segundo parametro  (inteiro) */
   movl  $0, %eax
   call  printf       /* chama a funcao da biblioteca */
 /*************************************************************/
-#pc++ 
-    incq %rbx
-    jmp loop_while
-FORA_WHILE:
-  #printf("\n");
-  #printf
+Lpula_print:
+  addq  $1, %r12  /* r12 += 1; */
+  jmp  L1         /* goto L1; */
+
+L2:  
 /*************************************************************/
-    /* este trecho imprime o valor de %eax (estraga %eax)  */
-  movq    $S_termina, %rdi    /* primeiro parametro (ponteiro)*/
-  movl    %eax, %esi   /* segundo parametro  (inteiro) */
+/* este trecho imprime o \n (estraga %eax)                  */
+  movq    $Sf2, %rdi    /* primeiro parametro (ponteiro)*/
   movl  $0, %eax
   call  printf       /* chama a funcao da biblioteca */
 /*************************************************************/
 
-#return 0;
 /***************************************************************/
 /* mantenha este trecho aqui e nao mexa - finalizacao!!!!      */
   movq  $0, %rax  /* rax = 0  (valor de retorno) */
@@ -80,5 +68,3 @@ FORA_WHILE:
   leave
   ret      
 /***************************************************************/
-
-#}
